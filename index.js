e2e8bbc96cf8807019fc17b852113c06caa2e9f7
adao74 <andreaminghweidao@hotmail.com>
2024-07-24 21:01:24 -0500

    add function for getting facts, index.html

---
+++ index.js
@@ -0,0 +1,40 @@
+let userInput;
+
+document.getElementById("user").addEventListener("keyup", (e) => {
+    userInput = e.target.value
+})
+
+document.getElementById("button").addEventListener("click", (e) => {
+    e.preventDefault()
+
+    getFacts()
+
+})
+
+const catchError = (res) => {
+    
+    // console.log(res)
+
+    if(!res.ok) {
+        console.log("There's an error")
+        throw Error(res.statusText)  // res.statusText is passed into catch as the argument 
+    } 
+    
+
+    return res.json()
+}
+
+
+const getFacts = () => {
+    fetch(`https://catfact.ninja/facts?limit=${userInput}&max_length=150`)
+    .then( (response) => catchError(response))
+    .then(res => {
+        console.log(res)
+        const newElement = document.createElement("p")
+        newElement.innerText = res.data[0].fact
+        document.getElementById("facts").append(newElement)
+    })
+    .catch(err => console.log(`Error,  ${err}`)) // for all errors
+  }
+
+
